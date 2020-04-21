426964a0a69504d8e6ce19fec66717327266c321
Kyaw Thi Ha <kywthiha@github.com>
2020-04-21 17:24:40 +0630

    BookStore User and Admin Filter Add Feacture

--- BookStore/Pages/Admin/Books/Index.cshtml.cs
+++ BookStore/Pages/Admin/Books/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BookStore.Data;
 using BookStore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookStore.Pages.Admin.Books
 {
@@ -20,11 +21,49 @@ namespace BookStore.Pages.Admin.Books
         }
 
         public IList<Book> Book { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Authors { get; set; }
+        public SelectList Categories { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BookAuthor { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BookCategory { get; set; }
 
         public async Task OnGetAsync()
         {
-            Book = await _context
-                .Book
+            var books = from b in _context.Book
+                        join c in _context.Category on b.CategoryID equals c.ID
+                        join a in _context.Author on b.AuthorID equals a.ID
+                        select b;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(b => b.Title.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(BookAuthor))
+            {
+                Int64 tempid = Convert.ToInt64(BookAuthor);
+                books = books.Where(b => b.AuthorID == tempid);
+            }
+            if (!string.IsNullOrEmpty(BookCategory))
+            {
+                Int64 tempid = Convert.ToInt64(BookCategory);
+                books = books.Where(b => b.CategoryID == tempid);
+            }
+            var authorsQuery = from a in _context.Author
+                               orderby a.Name
+                               select a;
+
+            Authors = new SelectList(authorsQuery.AsNoTracking(),
+                        "ID", "Name");
+            var categoriesQuery = from c in _context.Category
+                                  orderby c.Title
+                                  select c;
+
+            Categories = new SelectList(categoriesQuery.AsNoTracking(),
+                        "ID", "Title");
+            Book = await books
                 .Include(b=>b.Author)
                 .Include(b=>b.Category)
                 .AsNoTracking()
--- BookStore/Pages/Index.cshtml.cs
+++ BookStore/Pages/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using BookStore.Data;
 using BookStore.Models;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookStore.Pages
 {
@@ -23,6 +24,15 @@ namespace BookStore.Pages
         public IList<Book> Book { get;set; }
         public string BookCatsInfo { get; set; }
         public IDictionary<string, int> BookCats { get ; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Authors { get; set; }
+        public SelectList Categories { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BookAuthor { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BookCategory { get; set; }
 
         public async Task<ActionResult> OnGetAsync(int? id,int? clearData)
         {
@@ -54,8 +64,38 @@ namespace BookStore.Pages
                 
             }
             BookCatsInfo = SessionExtensions.GetCounts(HttpContext.Session).ToString();
-            Book = await _context
-                .Book
+
+            var books = from b in _context.Book
+                        join c in _context.Category on b.CategoryID equals c.ID
+                        join a in _context.Author on b.AuthorID equals a.ID
+                        select b;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(b=>b.Title.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(BookAuthor))
+            {
+                Int64 tempid = Convert.ToInt64(BookAuthor);
+                books = books.Where(b=>b.AuthorID == tempid);
+            }
+            if (!string.IsNullOrEmpty(BookCategory))
+            {
+                Int64 tempid = Convert.ToInt64(BookCategory);
+                books = books.Where(b => b.CategoryID == tempid);
+            }
+            var authorsQuery = from a in _context.Author
+                                  orderby a.Name
+                                  select a;
+
+            Authors = new SelectList(authorsQuery.AsNoTracking(),
+                        "ID", "Name");
+            var categoriesQuery = from c in _context.Category
+                                  orderby c.Title
+                                  select c;
+
+            Categories = new SelectList(categoriesQuery.AsNoTracking(),
+                        "ID", "Title");
+            Book = await books
                 .Include(b=>b.Author)
                 .Include(b=>b.Category)
                 .AsNoTracking()
